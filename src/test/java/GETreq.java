b430fd133996eafd2e37f011ae574fad76885d91
rncfg <58940453+rncfg@users.noreply.github.com>
2020-04-01 12:02:43 +0300

    Initial commit

---
+++ src/test/java/GETreq.java
@@ -0,0 +1,32 @@
+import static io.restassured.RestAssured.get;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class GETreq {
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://reqres.in";
+    }
+
+    @Test(description = "GET")
+    public void getRequestExampleTest() throws JSONException {
+        Response response = get("/api/users?page=2");
+                      int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+
+    }
+    @Test(description = "GET")
+    public void getRequestNegativeTest() throws JSONException{
+        Response response = get("/api/users");
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 404);
+    }
+
+}
---
+++ src/test/java/POSTreq.java
@@ -0,0 +1,37 @@
+package com.example.tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Date;
+
+public class POSTreq {
+
+    @Test(description = "POST")
+    public void postRequestExampleTest() {
+
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("name", "morpheus");
+        requestBody.put("job", "leader");
+
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        request.body(requestBody.toString());
+        Response response = request.post("https://reqres.in/api/users");
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 201);
+        String name = response.jsonPath().get("name");
+        Assert.assertEquals(name, "morpheus");
+        String job = response.jsonPath().get("job");
+        Assert.assertEquals(job, "leader");
+
+        System.out.println(response.getBody().asString());
+    }
+
+}
